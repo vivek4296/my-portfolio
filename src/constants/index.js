f73ec4a0c40926a32fce28e11935e227c7bd2257
vivek_chauhan <vivekc@adobe.com>
2025-02-24 22:56:12 +0530

    removing projects information

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,3 @@
-import project1 from "../assets/projects/project-1.webp";
-import project2 from "../assets/projects/project-2.webp";
-import project3 from "../assets/projects/project-3.webp";
-import project4 from "../assets/projects/project-4.webp";
 import {
   SiLeetcode,
   SiHackerrank,
@@ -85,37 +81,6 @@ preferences.
 • Used Hadoop to ingest CSV files and Hive for data querying.
 • Visualized data using Tableau for clear, actionable insights.`,
     technologies: ["Hadoop", "R", "Tableu", "Hive"],
-  },
-];
-
-export const PROJECTS = [
-  {
-    title: "E-Commerce Website",
-    image: project1,
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
   },
 ];
 
